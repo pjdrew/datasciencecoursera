fb48d8c9e6043689ca5efc906b0884b8f4bbc3bd
Peter Drew <peter.drew@kaplan.com>
2015-08-23 13:15:58 -0400

    add R script

--- 03 - Getting and Cleaning Data/run_analysis.R
+++ 03 - Getting and Cleaning Data/run_analysis.R
@@ -1,7 +1,43 @@
-## Merges the training and the test sets to create one data set.
-## Extracts only the measurements on the mean and standard deviation for each measurement. 
-## Uses descriptive activity names to name the activities in the data set
-## Appropriately labels the data set with descriptive variable names. 
-## From the data set in step 4, creates a second, independent tidy data set 
-##  with the average of each variable for each activity and each subject.
+library(dplyr)
+
+setwd("~/edu/datasciencecoursera/03 - Getting and Cleaning Data")
+unzip("getdata-projectfiles-UCI HAR Dataset.zip")
+
+feat <- read.table("UCI HAR Dataset/features.txt", stringsAsFactors = FALSE)
+
+x.test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = feat[, 2], check.names = FALSE)
+x.train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = feat[, 2], check.names = FALSE)
+x.test$ID <- seq.int(nrow(x.test))
+x.train$ID = seq.int(nrow(x.train))
+
+y.test <- read.table("UCI HAR Dataset/test/Y_test.txt", col.names = "activity.ID")
+y.train <- read.table("UCI HAR Dataset/train/Y_train.txt", col.names = "activity.ID")
+y.test$ID <- seq.int(nrow(y.test))
+y.train$ID <- seq.int(nrow(y.train))
+
+act.labels <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("activity.ID", "activity"))
+
+subj.test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subj.ID")
+subj.train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subj.ID")
+subj.test$ID <- seq.int(nrow(subj.test))
+subj.train$ID <- seq.int(nrow(subj.train))
+
+y.train.labeled <- merge(y.train, act.labels, by = "activity.ID")
+clean.x.train <- x.train[, !duplicated(colnames(x.train))]
+train <- merge(clean.x.train, y.train.labeled, by = "ID")
+
+y.test.labeled <- merge(y.test, act.labels, by = "activity.ID")
+clean.x.test <- x.test[, !duplicated(colnames(x.test))]
+test <- merge(clean.x.test, y.test.labeled, by = "ID")
+
+train <- merge(train, subj.train, by = "ID")
+test <- merge(test, subj.test, by = "ID")
+
+merged.sets <- rbind(test, train)
+
+merged.sets.extracted <- select(merged.sets, activity, subj.ID, contains("-mean()"), contains("-std()"))
+
+tidy.data <- merged.sets.extracted %>% group_by(activity, subj.ID) %>% summarise_each(funs(mean))
+
+write.table(tidy.data, "uci_har_tidy.txt")
 
